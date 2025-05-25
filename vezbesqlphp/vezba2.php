143e1da5d0fe817939fdea5747d2bb4fe1272efb
Djordje Stojkovic <djole8793@gmail.com>
2025-05-26 00:14:22 +0200

    Update vezba2.php

--- vezbesqlphp/vezba2.php
+++ vezbesqlphp/vezba2.php
@@ -1,6 +1,18 @@
 <?php
 
 $baza = mysqli_connect("localhost", "root", "", "prvi_cas");
+if(! isset($_POST['product_name']) || empty($_POST['product_name']))
+{
+    die("Niste uneli proizvod ");
+}
+elseif(! isset($_POST['price'])|| empty($_POST['price']))
+{
+    die("Niste uneli cenu");
+}
+elseif(! isset($_POST['category'])|| empty($_POST['category']))
+{
+    die("Niste uneli kategoriju");
+}
 
 $product_name = $_POST['product_name'];
 $price = $_POST['price'];
