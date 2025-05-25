02ef1a6d52fd34369f402764bd9ba602e026ff79
Djordje Stojkovic <djole8793@gmail.com>
2025-05-25 17:42:00 +0200

    Update domacisql.php

--- DOMACISQL/domacisql.php
+++ DOMACISQL/domacisql.php
@@ -1,4 +1,7 @@
 <?php
+//Napravio sam novu bazu podataka da bih otezao sebi da vidim kako bi to izgledalo sve
+//iz pocetka, inace uradio sam sve ovako samo sam zaboravio da napisem isset proveru to sam dodao kasnije
+//inace je sve radilo zapamtio sam proces :D
 $baza = mysqli_connect("localhost", "root", "", "domaci");
 
 if(! isset($_POST['email']) || empty($_POST['email']))
