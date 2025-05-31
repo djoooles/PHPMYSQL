8058d1baca795968e6637d0f6580a367ea478827
Djordje Stojkovic <djole8793@gmail.com>
2025-05-31 12:16:03 +0200

    Update domacisql.php

--- DOMACISQL/domacisql.php
+++ DOMACISQL/domacisql.php
@@ -8,14 +8,27 @@ if(! isset($_POST['email']) || empty($_POST['email']))
 {
     die("Niste uneli email");
 }
-elseif(! isset($_POST['sifra'])|| empty($_POST['sifru']))
+elseif(! isset($_POST['sifra'])|| empty($_POST['sifra']))
 {
     die("Niste uneli sifru");
 }
 $email = $_POST['email'];
-$lozinka = $_POST['sifra'];
+$lozinka = password_hash($_POST['sifra'],PASSWORD_BCRYPT);
 
-$baza->query("INSERT INTO upiszadomaci (email,sifra) VALUES ('$email', '$lozinka')");
+$rezultat = $baza->query(" SELECT * FROM upiszadomaci WHERE email = '$email' ");
+
+if($rezultat ->num_rows == 1)
+{
+    die("Vec postoji korisnik sa ovom email adresom");
+}
+else
+{
+echo "Uspesno ste se registrovali";
+$baza->query("INSERT INTO upiszadomaci (email,sifra) VALUES ('$email', '$lozinka')"); 
+}
+
+
+
 
 
 
